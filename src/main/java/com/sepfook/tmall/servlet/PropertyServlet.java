821b2d0c12b3c32e4fa880a84f7795d80d982b07
sepfook <sep_foo@163.com>
2024-08-18 10:49:51 +0800

    Changes

---
+++ src/main/java/com/sepfook/tmall/servlet/PropertyServlet.java
@@ -0,0 +1,33 @@
+package com.sepfook.tmall.servlet;
+
+import com.sepfook.tmall.util.Page;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PropertyServlet extends BaseBackServlet{
+    @Override
+    public String add(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+
+    @Override
+    public String delete(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+
+    @Override
+    public String edit(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+
+    @Override
+    public String update(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+
+    @Override
+    public String list(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+}
